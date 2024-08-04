7b20c2ad5cd77d91b74b43fbe771a77055b966a1
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-04 23:16:31 +0900

    Title: 더 긴 문자열, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/더 긴 문자열/longer-string.py
+++ 240804/더 긴 문자열/longer-string.py
@@ -2,6 +2,8 @@ s = list(input().split(' '))
 ss = ''
 if len(s[0]) > len(s[1]):
     ss = s[0]
-else:
+elif len(s[0]) < len(s[1]):
     ss = s[1]
+else:
+    ss = 'same'
 print(ss, max(len(s[0]), len(s[1])))
