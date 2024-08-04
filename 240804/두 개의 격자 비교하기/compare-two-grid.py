174141ba19527b35760f0dfdb7b3bf85168a1630
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-04 22:06:22 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split(' '))
+
+lst1 = [
+    list(map(int,input().split(' ')))
+    for _ in range(n)
+
+]
+
+lst2 = [
+    list(map(int,input().split(' ')))
+    for _ in range(n)
+
+]
+answers = []
+for i in range(n):
+    temp = []
+    for j in range(m):
+        if lst1[i][j] == lst2[i][j]:
+            temp.append(0)
+        else:
+            temp.append(1)
+    print(*temp)
