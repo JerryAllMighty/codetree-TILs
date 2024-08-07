1d65082ae3e417f50c6ea5b5ccdf4a45a0f3d203
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-08 00:13:58 +0900

    Title: 출국심사대 통과하기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/출국심사대 통과하기/passing-the-immigration-checkpoint.py
+++ 240808/출국심사대 통과하기/passing-the-immigration-checkpoint.py
@@ -3,8 +3,9 @@ lst = []
 for i in range(n):
     arriveTime, dueTime = map(int, input().split(' '))
     lst.append([arriveTime, dueTime])
-answer = lst[0][0]
+
 lst.sort(key=lambda x:x[0])
+answer = lst[0][0]
 for idx in range(len(lst)):
     if answer < lst[idx][0]:
         answer = lst[idx][0]
