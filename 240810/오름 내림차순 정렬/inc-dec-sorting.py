4686c0b3ef9b839a8fa83dd8447c97408d273de8
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-10 17:07:15 +0900

    Title: 오름 내림차순 정렬, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240810/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240810/오름 내림차순 정렬/inc-dec-sorting.py
@@ -1,6 +1,10 @@
 n = int(input())
 
-lst = list(map(int, input().split(' ')))
+lst2 = input().split(' ')
+lst = []
+for i in lst2:
+    lst.append(int(i))
+
 lst.sort()
 print(*lst)
 lst.sort(reverse=True)
