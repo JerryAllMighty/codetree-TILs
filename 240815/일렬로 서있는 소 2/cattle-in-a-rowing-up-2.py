f873a045e51f9f3f6714fa4191443add9def18db
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-15 12:43:27 +0900

    Title: 일렬로 서있는 소 2, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240815/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240815/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,10 +1,12 @@
 n = int(input())
 lst = list(map(int, input().split()))
 answer = 0
+answerList = []
 for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if i < j < k and lst[i] <= lst[j] <= lst[k] and lst[i] != lst[j] and lst[i] != lst[k] and lst[j] != lst[k]:
-                answer += 1
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if lst[i] <= lst[j] <= lst[k]:
+                each = (lst[i],lst[j],lst[k])
+                answerList.append(each)
 
-print(answer)
+print(len(answerList))
