d78525cd368db6a57751a3cc71088eb94026bfe6
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-15 18:21:27 +0900

    Title: 함수를 이용한 소수 판별, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240815/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240815/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,13 +2,21 @@ lst = input().split()
 a = int(lst[0])
 b = int(lst[1])
 answer = 0
-for i in range(a, b+1):
-   isPrime = True
-   for j in range(2,i):
-      if i % j == 0:
-         isPrime = False
 
-   if isPrime is True:
-      answer += i
+
+def isPrimeNum(num):
+    isPrime = True
+    if num < 3:
+        return True
+    else:
+        for j in range(2, num):
+            if num % j == 0:
+                isPrime = False
+    return isPrime
+
+
+for i in range(a, b + 1):
+    if isPrimeNum(i):
+        answer += i
 
 print(answer)
