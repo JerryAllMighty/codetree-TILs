c71d14b663e567b2d6eee2ac35f8c59ffba6d920
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-15 12:31:49 +0900

    Title: 일렬로 서있는 소 2, Time: 147ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240815/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240815/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -4,7 +4,7 @@ answer = 0
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            if i < j < k and lst[i] <= lst[j] <= lst[k] and lst[i] != lst[j] != lst[k]:
+            if i < j < k and lst[i] <= lst[j] <= lst[k] and lst[i] != lst[j] and lst[i] != lst[k] and lst[j] != lst[k]:
                 answer += 1
 
 print(answer)
