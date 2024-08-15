385f10168b81ce5714e50953672206c728a1b8d9
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-08-15 12:31:07 +0900

    Title: 일렬로 서있는 소 2, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240815/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = list(map(int, input().split()))
+answer = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if i < j < k and lst[i] <= lst[j] <= lst[k] and lst[i] != lst[j] != lst[k]:
+                answer += 1
+
+print(answer)
