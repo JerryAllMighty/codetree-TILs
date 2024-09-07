527c2ad633f51ad66eccadae0acb857273c0710e
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-07 11:10:51 +0900

    Title: Date to Date, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240907/Date to Date/date-to-date.py
@@ -0,0 +1,20 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+days = 0
+
+
+def countDays(month):
+    result = 0
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        result += 31
+    elif month == 2:
+        result += 28
+    else:
+        result += 30
+    return result
+
+
+for i in range(m1, m2):
+    days += countDays(i)
+
+print(days - d1 + d2 + 1)
