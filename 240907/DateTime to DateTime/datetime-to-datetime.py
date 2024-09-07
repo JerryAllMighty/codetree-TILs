35a3914c8b66a5be279fc2de8e55f5a3859f7eaf
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-07 11:35:14 +0900

    Title: DateTime to DateTime, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240907/DateTime to DateTime/datetime-to-datetime.py
+++ 240907/DateTime to DateTime/datetime-to-datetime.py
@@ -2,7 +2,7 @@ day, hour, minute = map(int, input().split())
 
 if (((day) * 24 * 60) + ((hour) * 60) + (minute )) < ((11 * 24 * 60) + (11 * 60) + 11):
     print(-1)
-    exit(1)
+    exit(0)
 else:
     dayGap = day - 11
     hourGap = hour - 11
