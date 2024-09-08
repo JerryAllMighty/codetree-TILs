019717cc5fef25131a507bb8612f999f5d5a283f
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-08 17:57:38 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240908/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -3,16 +3,24 @@ n = int(input())
 lst = [0 for _ in range(201)]
 
 current = 0
+lastDirection = ''
 for _ in range(n):
     cnt, direction = input().split()
-    for i in range(int(cnt)):
-        idx = 100 + current
-        if idx > 200:
-            idx = 200
-        lst[idx] += 1
+    if lastDirection == 'R':
+        if direction == 'L':
+            current -= 1
+    elif lastDirection == 'L':
         if direction == 'R':
             current += 1
+
+    for i in range(int(cnt)):
+        if direction == 'R':
+            lst[100 + current] += 1
+            current += 1
         else:
+            lst[100 + current] += 1
             current -= 1
+
+    lastDirection = direction
 
 print(len([i for i in lst if i >= 2]))
