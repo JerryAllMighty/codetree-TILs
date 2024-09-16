88c6269880a33f607be69fe865b0453f9f492718
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-16 13:26:56 +0900

    Title: 개발자의 컴퓨터, Time: 118ms, Memory: 27MB, Status: Passed - CodeTree

--- 240916/개발자의 컴퓨터/developers-computers.py
+++ 240916/개발자의 컴퓨터/developers-computers.py
@@ -1,5 +1,5 @@
 n = int(input())
-lst = [0 for i in range(1000)]
+lst = [0 for i in range(1000*n)]
 for _ in range(n):
     start, end, cnt = map(int, input().split())
     for i in range(start, end+1):
