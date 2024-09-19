8d27d760888848ac333b501a2e3cb6deb3006788
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-19 22:39:37 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 447ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240919/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240919/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -6,25 +6,18 @@ lst = [
 ]
 offset = 1000
 x1, y1, x2, y2 = map(int, input().split())
-for i in range(x1, x2 + 1):
-    for j in range(y1, y2 + 1):
+for i in range(x1, x2):
+    for j in range(y1, y2):
         lst[i + offset][j + offset] = 2
 
 x3, y3, x4, y4 = map(int, input().split())
 
-for i in range(x3, x4 + 1):
-    for j in range(y3, y4 + 1):
+for i in range(x3, x2):
+    for j in range(y1,y4):
         lst[i + offset][j + offset] -= 1
 
-rows = []
-cols = []
-for i in range(len(lst)):
-    for j in range(len(lst[i])):
-        if lst[i][j] == 2:
-            rows.append(i)
-            cols.append(j)
-maxRow = max(rows) if rows else 0
-minRow = min(rows) if rows else 0
-maxCol = max(cols) if cols else 0
-minCol = min(cols) if cols else 0
-print((maxRow - minRow) * (maxCol - minCol))
+answer = 0
+for i in lst:
+    answer += collections.Counter(i).get(2) if collections.Counter(i).get(2) is not None else 0
+
+print(answer + ((x2-x3) * (y4-y1)))
