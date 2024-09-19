b22684922bf63ac76e585ddd00e6480b3b3acea5
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-19 21:46:00 +0900

    Title: 색종이의 총 넓이, Time: 545ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240919/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -0,0 +1,19 @@
+import collections
+
+n = int(input())
+lst = [
+    [0 for i in range(n * 100)]
+    for _ in range(n * 100)
+]
+cnt = n
+while (cnt > 0):
+    x, y = map(int, input().split())
+    for i in range(x, x + 8):
+        for j in range(y, y + 8):
+            lst[i][j] = 1
+    cnt -= 1
+answer = 0
+for i in lst:
+    answer += collections.Counter(i).get(1) if collections.Counter(i).get(1) is not None else 0
+
+print(answer)
