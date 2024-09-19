b5485a7aff841a3680f692408de1199469dd0916
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-09-19 22:32:24 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 205ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240919/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240919/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -6,15 +6,15 @@ lst = [
 ]
 offset = 1000
 x1, y1, x2, y2 = map(int, input().split())
-for i in range(x1, x2+1):
-    for j in range(y1, y2+1):
-        lst[i+offset][j+offset] = 2
+for i in range(x1, x2 + 1):
+    for j in range(y1, y2 + 1):
+        lst[i + offset][j + offset] = 2
 
 x3, y3, x4, y4 = map(int, input().split())
 
-for i in range(x3, x4+1):
-    for j in range(y3, y4+1):
-        lst[i+offset][j+offset] -= 1
+for i in range(x3, x4 + 1):
+    for j in range(y3, y4 + 1):
+        lst[i + offset][j + offset] -= 1
 
 rows = []
 cols = []
@@ -23,5 +23,8 @@ for i in range(len(lst)):
         if lst[i][j] == 2:
             rows.append(i)
             cols.append(j)
-
-print((max(rows) - min(rows)) * (max(cols) - min(cols)))
+maxRow = max(rows) if rows else 0
+minRow = min(rows) if rows else 0
+maxCol = max(cols) if cols else 0
+minCol = min(cols) if cols else 0
+print((maxRow - minRow) * (maxCol - minCol))
