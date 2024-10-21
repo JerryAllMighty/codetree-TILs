2d7b6258dd870baf80d8215da2805c20d0873f4c
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-10-21 13:37:53 +0900

    Title: 달리자, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/달리자/run-and-run.py
+++ 241021/달리자/run-and-run.py
@@ -3,18 +3,11 @@ answer = 0
 lst = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
 
-
-def findSmall(num):
-    for i in range(n):
-        if lst[i] + num <= lst2[i]:
-            return i
-    return -1
-
-
-for i in range(len(lst)):
+for i in range(n):
     if lst[i] > lst2[i]:
         gap = lst[i] - lst2[i]
-        idx = findSmall(gap)
-        answer += (gap * (idx - i))
+        lst[i+1] += gap
+        lst[i] -= gap
+        answer += gap
 
 print(answer)
