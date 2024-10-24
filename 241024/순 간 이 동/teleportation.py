74d67ac076025a35fe5a9b7e732e22d55d158899
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-10-24 21:49:03 +0900

    Title: 순 간 이 동, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 241024/순 간 이 동/teleportation.py
+++ 241024/순 간 이 동/teleportation.py
@@ -1,14 +1,5 @@
 a, b, x, y = map(int, input().split())
-q=y-x
-w=x-a+y-b
-e=x-a+y-x
-r=a-x+b-y
-if q < 0:
-    q=100 ** 4
-if w < 0:
-    w = 100 ** 4
-if e < 0:
-    e = 100 ** 4
-if r < 0:
-    r = 100 ** 4
-print(min(q,w,e,r))
+q=abs(b-a)
+w=abs(x-a)+abs(b-y)
+e=abs(y-a)+abs(b-x)
+print(min(q,w,e))
