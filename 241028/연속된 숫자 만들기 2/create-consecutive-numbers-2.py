cbe291b602eae74fd5d7bda93679c2959d23c0b4
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-10-28 22:52:53 +0900

    Title: 연속된 숫자 만들기 2, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 241028/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241028/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -2,15 +2,17 @@ lst = list(map(int, input().split()))
 
 
 def isContinuous(targetList):
-    if targetList[0] + 1 == targetList[1] and targetList[1] + 1 == targetList[2]:
+    minNum = min(targetList)
+    if (minNum + 1 in targetList) and (minNum + 2 in targetList):
         return True
     return False
 
 
 def isAlmostContinuous(targetList):
-    if ((targetList[0] + 2) == targetList[1]) or ((targetList[1]+2) == targetList[2]):
+    if ((targetList[0] + 2) == targetList[1]) or ((targetList[1] + 2) == targetList[2]):
         return True
     return False
+
 
 if isContinuous(lst):
     print(0)
