2fa2e27967e57796d5fdead8dc3ae7bd91aea30b
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-12-21 21:05:25 +0900

    Title: 두 정수에 대한 연산값 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241221/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 241221/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
 public class Main {
-    static String calc(Integer a, Integer b) {
-        if(a > b){
-            b = b + 10;
-            a = a * 2;
-        }else{
-            a = a + 10;
-            b = b *2;
+    static class IntWrapper{
+        int val = 0;
+
+        IntWrapper(int a){
+            this.val = a;
+
         }
-        String[] arr = new String[2];
-        arr[0] = Integer.toString(a);
-        arr[1] = Integer.toString(b);
-        return String.join(" ", arr);
+    }
+    static void calc(IntWrapper a, IntWrapper b) {
+        if(a.val > b.val){
+            b.val += 10;
+            a.val *= 2;
+        }else{
+            a.val += 10;
+            b.val *= 2;
+        }
     }
 
     public static void main(String[] args) {
@@ -20,8 +24,12 @@ public class Main {
         String[] input = sc.nextLine().split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
-        String calcList = calc(a, b);
-        System.out.println(calcList);
+        IntWrapper intA = new IntWrapper(a);
+        IntWrapper intB = new IntWrapper(b);
+        calc(intA, intB);
+        input[0] = Integer.toString(intA.val);
+        input[1] = Integer.toString(intB.val);
+        System.out.println(String.join(" ",input));
 //        System.out.println(String.join(" ",));
 
     }
