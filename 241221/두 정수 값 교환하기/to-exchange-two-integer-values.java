b044513aea3a4ec0859df1628d6c2318f36b069a
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-12-21 16:20:57 +0900

    Title: 두 정수 값 교환하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241221/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String[] input = sc.nextLine().split(" ");
+        String a = input[0];
+        String b = input[1];
+        System.out.println(b + " " + a);
+    }
+}
