cbbd3e14141a888483e103bb4f59a5dfec258952
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2024-12-21 20:30:42 +0900

    Title: 2개 이상의 알파벳, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241221/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 241221/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -2,20 +2,25 @@ import java.util.*;
 
 public class Main {
     static String moreThanTwo(String s) {
-        String result = "No";
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            String str = s.substring(i,i+1);
+            String str = s.substring(i, i + 1);
             Integer val = map.get(str);
-            if(val == null){
-                map.put(str,1);
-            }else{
-                if(val+1 >= 2){
-                    return "Yes";
+            if (val == null) {
+                map.put(str, 1);
+            } else {
+                if (val + 1 >= 2) {
+                    map.put(str, val + 1);
                 }
             }
         }
-        return result;
+        int mapKeySize = map.keySet().size();
+        if(mapKeySize < 2){
+            return "No";
+        }else{
+            return "Yes";
+        }
+
     }
 
     public static void main(String[] args) {
